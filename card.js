80125d7821add66f6474f17a3a9a0214bbec03da
IliaBeridze <106550587+IliaBeridze@users.noreply.github.com>
2023-03-12 17:59:22 +0400

    feat card

---
+++ card.js
@@ -0,0 +1,18 @@
+export default function ShopItem(props) {
+  return (`<div class="card">
+  <img
+    class="card-img"
+    src="${props.img}"
+  />
+
+  <div class="card-info">
+    <div class="card-category">${props.category}</div>
+
+    <div class="card-title">${props.title}</div>
+    <div class="card-description">
+      ${props.description}
+    </div>
+    <div class="card-price">${props.price}$</div>
+  </div>
+</div>`)
+}
--- main.js
+++ main.js
@@ -1,1 +1,45 @@
 import "./style.scss";
+import "./card.scss";
+import Card from "./card";
+//create card component 1
+//img, category, title, description, price
+
+const app = document.querySelector("#app");
+
+const mockData = [
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA_djpm0QdOAgvKxQhB3EZuT6JPHn1FcSDaw&usqp=CAU",
+    category: "Trousers",
+    title: "Stretch Wool Chino Trousers",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor nunc nibh, in posuere arcu malesuada.",
+    price: 199,
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPFw53-E6Th5jalJduzZbeKpmNYdU2F7_6vA&usqp=CAU",
+    category: "Trousers",
+    title: "Stretch Wool Chino Trousers",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor nunc nibh, in posuere arcu malesuada.",
+    price: 199,
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyWesXONYQaVCjyPORaGhdIgTYfuPM2_DkHQ&usqp=CAU",
+    category: "Trousers",
+    title: "Stretch Wool Chino Trousers",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor nunc nibh, in posuere arcu malesuada.",
+    price: 199,
+  },
+];
+
+console.log(mockData);
+
+function renderItems() {
+  mockData.forEach((item) => {
+    // console.log(item.img);
+    app.innerHTML += Card(item);
+    
+  });
+}
+renderItems();
